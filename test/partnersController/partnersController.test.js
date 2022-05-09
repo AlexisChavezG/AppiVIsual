91e922f95c10ee865bffbd4769cccb14acb41f00
AlexisChavezG <alexis.chavez94@outlook.com>
2022-05-08 20:57:59 -0500

    Agrego prueba unitaria para el endPoint de email

--- test/partnersController/partnersController.test.js
+++ test/partnersController/partnersController.test.js
@@ -1,8 +1,13 @@
-const { expect } = require('@jest/globals')
+const { expect, test } = require('@jest/globals')
 const control = require('../../lib/controllers/partnersController')
 describe("Pruebas automatizadas",()=>{
     test("1. Prueba para obtener todos los datos",()=>{
         const result = control.visualpartners()
         expect(result).toBeDefined()
+    });
+    test("2. Prueba para los correos",()=>{
+        const emailPrueba = "Todd@visualpartnership.xyz";
+        const listEmail = control.getEmailData();
+        expect(listEmail).toBe(emailPrueba);
     })
 })
