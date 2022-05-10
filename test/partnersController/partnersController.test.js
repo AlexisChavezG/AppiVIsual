0aa38f0d2e93694555c4cc331cebe66bf2f71485
AlexisChavezG <alexis.chavez94@outlook.com>
2022-05-10 16:11:47 -0500

    Se crea las funciones para el controlador y el server para filtrar los registros con el campo credits mayor a 500

--- test/partnersController/partnersController.test.js
+++ test/partnersController/partnersController.test.js
@@ -1,13 +1,13 @@
-const { expect, test } = require('@jest/globals')
-const control = require('../../lib/controllers/partnersController')
+const { expect, test } = require("@jest/globals");
+const control = require("../../lib/controllers/partnersController");
 describe("Pruebas automatizadas",()=>{
     test("1. Prueba para obtener todos los datos",()=>{
-        const result = control.visualpartners()
-        expect(result).toBeDefined()
+        const result = control.visualpartners();
+        expect(result).toBeDefined();
     });
     test("2. Prueba para los correos",()=>{
         const emailPrueba = "Todd@visualpartnership.xyz";
         const listEmail = control.getEmailData();
         expect(listEmail.emails[0]).toBe(emailPrueba);
     });
-})
+});
